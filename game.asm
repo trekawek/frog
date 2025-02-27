b8eaa9727b27d54fa5e1bb4cab43b6677d714d6a
Tomek Rękawek <trekawek@gmail.com>
2025-02-27 16:37:44 +0100

    Restart game or go to the next level.

--- game.asm
+++ game.asm
@@ -5,6 +5,49 @@ init_game equ *
           sta tongues_bcd
           lda #flies_c
           sta remaining_flies
+          
+          lda #scr_minx
+          sta fly_1_obj
+          sta frog_obj
+          sta wasp_obj
+
+          lda #scr_minx+5
+          sta fly_2_obj
+
+          lda #scr_minx+10
+          sta fly_3_obj
+
+          lda #scr_minx+2
+          sta fly_4_obj
+
+          lda #scr_minx+7
+          sta fly_5_obj
+
+          lda #scr_minx+12
+          sta fly_6_obj
+
+          lda #scr_minx+5
+          sta fly_7_obj
+
+          lda #scr_minx+10
+          sta fly_8_obj
+
+          lda #scr_minx+15
+          sta fly_9_obj
+
+          lda #0
+          sta fly_1_obj+6
+          sta fly_2_obj+6
+          sta fly_3_obj+6
+          sta fly_4_obj+6
+          sta fly_5_obj+6
+          sta fly_6_obj+6
+          sta fly_7_obj+6
+          sta fly_8_obj+6
+          sta fly_9_obj+6
+          sta frog_obj+6
+          sta wasp_obj+6
+
           rts
 
 is_next_level equ *
@@ -14,6 +57,9 @@ is_next_level equ *
           jmp init_game
 
 is_game_over equ *
+          lda tngue_act
+          seq
+          rts
           lda tongues
           seq
           rts
